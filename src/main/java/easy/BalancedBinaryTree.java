dcb9240cd3ad154d597d39b88091cc20f64aeec6
max <maxim.kozlyuk@yandex.ru>
2024-11-10 21:10:23 +0300

    BalancedBinaryTree

---
+++ src/main/java/easy/BalancedBinaryTree.java
@@ -0,0 +1,51 @@
+package easy;
+
+public class BalancedBinaryTree {
+
+    // Definition for a binary tree node.
+    // cant change.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * https://leetcode.com/problems/balanced-binary-tree/
+     */
+    public boolean isBalanced(TreeNode root) {
+        return checkHeight(root) != -1;
+    }
+
+    private int checkHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = checkHeight(node.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = checkHeight(node.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+}
