7df3790571cf12c158358e1661655742ca2f7e54
max <maxim.kozlyuk@yandex.ru>
2025-01-12 17:07:32 +0300

    NumberOfIslands solution with cache

--- src/main/java/leetcode/medium/NumberOfIslands.java
+++ src/main/java/leetcode/medium/NumberOfIslands.java
@@ -7,6 +7,8 @@ public final class NumberOfIslands {
     private static final char LAND = '1';
     private static final char WATER = '0';
 
+    private Point[][] gridPointsCache;
+
     private record Point(int x, int y) {
         @Override
         public boolean equals(Object obj) {
@@ -22,6 +24,7 @@ public final class NumberOfIslands {
      * https://leetcode.com/problems/number-of-islands
      */
     public int numIslands(char[][] grid) {
+        gridPointsCache = new Point[grid.length][grid[0].length];
         var islandsCount = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
@@ -57,33 +60,42 @@ public final class NumberOfIslands {
         }
     }
 
-    // todo можно оптимизировать через pointOf(x,y) который сделает точки singleton-ами
     private List<Point> adjacentIslandPointsOf(char[][] grid, Point point) {
         var result = new ArrayList<Point>(4);
         // top
         var topY = point.y - 1;
         if (topY >= 0 && grid[point.x][topY] == LAND) {
-            result.add(new Point(point.x, topY));
+            result.add(pointOf(point.x, topY));
         }
 
         // right
         var rightX = point.x + 1;
         if (rightX < grid.length && grid[rightX][point.y] == LAND) {
-            result.add(new Point(rightX, point.y));
+            result.add(pointOf(rightX, point.y));
         }
 
         // bottom
         var bottomY = point.y + 1;
         if (bottomY < grid[0].length && grid[point.x][bottomY] == LAND) {
-            result.add(new Point(point.x, bottomY));
+            result.add(pointOf(point.x, bottomY));
         }
 
         // left
         var leftX = point.x - 1;
         if (leftX >= 0 && grid[leftX][point.y] == LAND) {
-            result.add(new Point(leftX, point.y));
+            result.add(pointOf(leftX, point.y));
         }
         return result;
+    }
+
+    private Point pointOf(int x, int y) {
+        var cached = gridPointsCache[x][y];
+        if (cached == null) {
+            gridPointsCache[x][y] = new Point(x, y);
+            return gridPointsCache[x][y];
+        } else {
+            return cached;
+        }
     }
 
 }
